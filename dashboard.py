885aa5a91a1664b76f380ae01900d440e2decfd7
jaina1008 <mailme.anshuljain@gmail.com>
2024-07-23 19:48:02 +1200

    download option added

--- dashboard.py
+++ dashboard.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import plotly.express as px
+import matplotlib
 import pandas as pd
 import os
 import warnings
@@ -73,9 +74,36 @@ else:
     filtered_df = df3[df3['Region'].isin(region) & df3['State'].isin(state) & df3['City'].isin(city)]
 
 
+# Charts
 category_df = filtered_df.groupby(by = ['Category'], as_index=False)['Sales'].sum()
 
+# Bar chart
 with col1:
     st.subheader('Category wise Sales')
     fig = px.bar(category_df, x='Category', y='Sales', text=['${:,.2f}'.format(x) for x in category_df['Sales']], template='seaborn')
     st.plotly_chart(fig, use_container_width=True, height=200)
+
+# Pie chart
+with col2:
+    st.subheader('Region wise Sales')
+    fig = px.pie(filtered_df, values='Sales', names='Region', hole=0.5)
+    fig.update_traces(text=filtered_df['Region'], textposition='outside')
+    st.plotly_chart(fig, use_container_width=200)
+
+
+# Download Selected Data
+cl1, cl2 = st.columns(2)
+with cl1:
+    with st.expander('Category_ViewData'):
+        st.write(category_df.style.background_gradient(cmap='Blues'))
+        csv=category_df.to_csv(index=False)
+        st.download_button('Download Data', data=csv, file_name='Category.csv', mime='text/csv',
+                           help='Click here to download selected data as a CSV file')
+
+with cl2:
+    with st.expander('Region_ViewData'):
+        region= filtered_df.groupby(by='Region', as_index=False)['Sales'].sum()
+        st.write(region.style.background_gradient(cmap='Oranges'))
+        csv=region.to_csv(index=False)
+        st.download_button('Download Data', data=csv, file_name='Region.csv', mime='text/csv',
+                           help='Click here to download selected data as a CSV file')
