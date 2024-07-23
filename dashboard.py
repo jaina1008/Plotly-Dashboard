5ae36008a32a0700e848000fa9197917530959be
jaina1008 <mailme.anshuljain@gmail.com>
2024-07-23 19:05:28 +1200

    filter completed

--- dashboard.py
+++ dashboard.py
@@ -34,6 +34,48 @@ with col2:
 df = df[(df['Order Date'] >= date1) & (df['Order Date'] <= date2)].copy()
 
 
-# Filter selection by user
+# Create Filter Sidebar
 st.sidebar.header('Choose your filter: ')
+
+# Filter Region by user
 region = st.sidebar.multiselect('Pick your region', df['Region'].unique())
+if not region:
+    df2 = df.copy()
+else:
+    df2 = df[df['Region'].isin(region)]
+
+# Filter State by user
+state = st.sidebar.multiselect('Pick your state', df2['State'].unique())
+if not state:
+    df3 = df2.copy()
+else:
+    df3 = df2[df2['State'].isin(state)]
+
+# Filter City by user
+city = st.sidebar.multiselect('Pick your city', df3['City'].unique())
+
+# Filter based on Region, State and City
+if not region and not state and not city:
+    filtered_df = df
+elif not state and not city:
+    filtered_df = df[df["Region"].isin(region)]
+elif not region and not city:
+    filtered_df = df[df["State"].isin(state)]
+elif state and city:
+    filtered_df = df3[df['State'].isin(state) & df3['City'].isin(city)]
+elif region and city:
+    filtered_df = df3[df['Region'].isin(region) & df3['City'].isin(city)]
+elif region and state:
+    filtered_df = df3[df['Region'].isin(region) & df3['State'].isin(state)]
+elif city:
+    filtered_df = df3[df3['City'].isin(city)]
+else:
+    filtered_df = df3[df3['Region'].isin(region) & df3['State'].isin(state) & df3['City'].isin(city)]
+
+
+category_df = filtered_df.groupby(by = ['Category'], as_index=False)['Sales'].sum()
+
+with col1:
+    st.subheader('Category wise Sales')
+    fig = px.bar(category_df, x='Category', y='Sales', text=['${:,.2f}'.format(x) for x in category_df['Sales']], template='seaborn')
+    st.plotly_chart(fig, use_container_width=True, height=200)
