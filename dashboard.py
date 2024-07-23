da5f69eb574ffd7210915f46a719fdc0e4f8b937
jaina1008 <mailme.anshuljain@gmail.com>
2024-07-23 17:06:52 +1200

    created setup page

--- dashboard.py
+++ dashboard.py
@@ -9,12 +9,12 @@ st.set_page_config(page_title='Superstore', page_icon=":bar_chart:", layout="wid
 st.title(":bar_chart: Sample SuperStore EDA")
 st.markdown('<style>div.block-container{padding-top:1.8rem;}</style>', unsafe_allow_html=True)
 
-# Browser or Upload Data
-file = st.file_uploader(':file_folder: Upload a file', type=(['csv', 'txt','xlsx','xls']))
-if file is not None:
-    fileName = file.name
-    st.write(fileName)
-    df = pd.read_csv(fileName)
-else:
-    os.chdir(f"./data")
-    df = pd.read_csv()
+# # Browser or Upload Data
+# file = st.file_uploader(':file_folder: Upload a file', type=(['csv', 'txt','xlsx','xls']))
+# if file is not None:
+#     fileName = file.name
+#     st.write(fileName)
+#     df = pd.read_csv(fileName)
+# else:
+#     os.chdir(f"./data")
+#     df = pd.read_csv()
