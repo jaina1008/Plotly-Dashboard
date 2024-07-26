9a4ee56c87596dabe2b2102f2edc29916004e2ea
jaina1008 <mailme.anshuljain@gmail.com>
2024-07-26 14:11:23 +1200

    myCharts.py setup initiated, requirements.txt updated

--- dashboard.py
+++ dashboard.py
@@ -1,23 +1,14 @@
-import os
+
 from dash import dcc, html, Dash, callback, Input, Output
-from src.codes import code
-import plotly.express as px
-import plotly.graph_objects as go
-import pandas as pd
+import src.myCharts as myCharts
 
 # bgColor="#e5ecf6"
 # margin=dict(l=10,r=10,t=50,b=10)
 # height=600
 # font=dict(size=10)
 
-SOURCE_PATH: str= os.path.join('data', 'storeData.csv')
 CSS=["https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css", ]
 NAME='TOGETHER DASHBOARD'
-
-
-dataFrame=pd.read_csv(SOURCE_PATH)
-dataFrame['Order Date'] = pd.to_datetime(dataFrame['Order Date'])
-df = pd.DataFrame(dataFrame)
 
 
 app = Dash(NAME, external_stylesheets=CSS)
@@ -60,111 +51,33 @@ app.layout = html.Div([
 
 ### 2. Callback Definition
 @app.callback(
-        Output('state-dropdown', 'options'), Input('region-dropdown', 'value')
-)
-def set_state_options(selected_region):
-    state_df=df.copy()
-    states=[]
-    if selected_region:
-        state_df= state_df[state_df['Region'].isin(selected_region)]
-        states= state_df['State'].unique()
-    else:
-        states= state_df['State'].unique()
-    return [{'label': states, 'value': states} for states in states]
-
+        Output('state-dropdown', 'options'), Input('region-dropdown', 'value'))
+def state_options(selected_region):
+    return myCharts.set_state_options(selected_region)
 
 @app.callback(
-        Output('city-dropdown', 'options'), Input('state-dropdown', 'value')
-)
-def set_city_options(selected_state):
-    city_df=df.copy()
-    cities=[]
-    if selected_state:
-        city_df= city_df[city_df['State'].isin(selected_state)]
-        cities= city_df['City'].unique()
-    else:
-        cities= city_df['City'].unique()
-    return [{'label': cities, 'value': cities} for cities in cities]
-
+        Output('city-dropdown', 'options'), Input('state-dropdown', 'value'))
+def city_option(selected_state):
+    return myCharts.set_state_options(selected_state)
 
 
 @app.callback(
         Output('Chart1', 'figure'), [Input('region-dropdown', 'value'), Input('state-dropdown', 'value'), Input('city-dropdown', 'value')])
 def update_chart_one(region, state, city):
-    return category_sales_chart(region, state, city)
+    return myCharts.category_sales_chart(region, state, city)
 
 @app.callback(
         Output('Chart2', 'figure'), [Input('region-dropdown', 'value'), Input('state-dropdown', 'value'), Input('city-dropdown', 'value')])
 def update_chart_two(region, state, city):
-    return subCategory_sales_chart(region, state, city)
+    return myCharts.subCategory_sales_chart(region, state, city)
 
 @app.callback(
         Output('Chart3', 'figure'), [Input('region-dropdown', 'value'), Input('state-dropdown', 'value')])
 def update_chart_three(region, state):
-    return create_choropleth_map(region, state)
+    return myCharts.create_choropleth_map(region, state)
 
 
 
-
-### 3. Plotly Function
-def category_sales_chart(region, state, city):
-    # Create a copy of the dataframe
-    filtered_df = df.copy()
-    # Filter by region, state, and city if provided
-    if region:
-        filtered_df = filtered_df[filtered_df['Region'].isin(region)]
-    if state:
-        filtered_df = filtered_df[filtered_df['State'].isin(state)]
-    if city:
-        filtered_df = filtered_df[filtered_df['City'].isin(city)]
-    
-    # Group by category and sum the sales
-    agg_df = filtered_df.groupby('Category', as_index=False)['Sales'].sum()
-    
-    # Create the figure using Plotly Express
-    fig = px.bar(agg_df, x='Category', y='Sales')
-    return fig
-
-
-def subCategory_sales_chart(region, state, city):
-    # Create a copy of the dataframe
-    filtered_df = df.copy()
-    # Filter by region, state, and city if provided
-    if region:
-        filtered_df = filtered_df[filtered_df['Region'].isin(region)]
-    if state:
-        filtered_df = filtered_df[filtered_df['State'].isin(state)]
-    if city:
-        filtered_df = filtered_df[filtered_df['City'].isin(city)]
-    
-    # Group by category and sum the sales
-    agg_df = filtered_df.groupby('Sub-Category', as_index=False)['Sales'].sum()
-    
-    # Create the figure using Plotly Express
-    fig = px.bar(agg_df, x='Sub-Category', y='Sales')
-    return fig
-
-
-def create_choropleth_map(region, state):
-    # Create a copy of the dataframe
-    filtered_df = df.copy()
-    # Filter by region, state, and city if provided
-    if region:
-        filtered_df = filtered_df[filtered_df['Region'].isin(region)]
-    if state:
-        filtered_df = filtered_df[filtered_df['State'].isin(state)] 
-    filtered_df['Code'] = filtered_df['State'].map(code)
-    filtered_df = filtered_df.groupby('Code', as_index=False)['Sales'].sum().round(2)
-
-    # Create a choropleth map
-    fig = px.choropleth(filtered_df, locations='Code', color='Sales', color_continuous_scale='spectral_r', hover_name='Code', locationmode='USA-states', labels={'State Sales'}, scope='usa')
-
-    # Update layout for better display
-    fig.update_layout(
-        title_text='Sales by US State',
-        geo=dict( scope='usa', lakecolor='rgb(255, 255, 255)', showlakes=True, showland=True, landcolor='rgb(217, 217, 217)', subunitcolor='rgb(255, 255, 255)'),
-        margin=dict(l=10,r=10,t=50,b=10))
-    return fig
 
 if __name__ == '__main__':
     app.run_server(debug=True)
---
+++ src/myCharts.py
@@ -0,0 +1,134 @@
+import os
+import sys
+from dataclasses import dataclass
+from src.exception import CustomException
+from src.codes import code
+import plotly.express as px
+import plotly.graph_objects as go
+import pandas as pd
+
+
+@dataclass
+class sourceConfig:
+    DATA_SOURCE: str= os.path.join('src','storeData.csv')
+
+class dataLoader:
+    def __init__(self):
+        self.SOURCE=sourceConfig()
+        self.data=None
+    
+    def load_data(self):
+        try:
+            file_extension=self.SOURCE.split('.')[-1].lower()
+            if file_extension=='csv':
+                self.data=self._load_csv()
+            elif file_extension=='txt':
+                self.data=self._load_txt()
+            elif file_extension in ['xls', 'xlsx']:
+                self.data=self._load_excel()
+        except Exception as e:
+                raise CustomException(e, sys)
+        
+        return self.data
+    
+    def _load_csv(self):
+        return pd.read_csv(self.SOURCE)
+    
+    def _load_txt(self):
+        with open(self.SOURCE, 'r') as obj:
+            content=obj.read()
+        return content
+    
+    def _load_excel(self):
+        return pd.read_excel(self.SOURCE)
+
+
+SOURCE_PATH: str= os.path.join('data', 'storeData.csv')
+dataFrame=pd.read_csv(SOURCE_PATH)
+dataFrame['Order Date'] = pd.to_datetime(dataFrame['Order Date'])
+df = pd.DataFrame(dataFrame)
+
+
+# Set Options based on app callbacks
+def set_state_options(selected_region):
+    state_df=df.copy()
+    states=[]
+    if selected_region:
+        state_df= state_df[state_df['Region'].isin(selected_region)]
+        states= state_df['State'].unique()
+    else:
+        states= state_df['State'].unique()
+    return [{'label': states, 'value': states} for states in states]
+
+
+def set_city_options(selected_state):
+    city_df=df.copy()
+    cities=[]
+    if selected_state:
+        city_df= city_df[city_df['State'].isin(selected_state)]
+        cities= city_df['City'].unique()
+    else:
+        cities= city_df['City'].unique()
+    return [{'label': cities, 'value': cities} for cities in cities]
+
+
+
+### 3. Plotly Function
+def category_sales_chart(region, state, city):
+    # Create a copy of the dataframe
+    filtered_df = df.copy()
+    # Filter by region, state, and city if provided
+    if region:
+        filtered_df = filtered_df[filtered_df['Region'].isin(region)]
+    if state:
+        filtered_df = filtered_df[filtered_df['State'].isin(state)]
+    if city:
+        filtered_df = filtered_df[filtered_df['City'].isin(city)]
+    
+    # Group by category and sum the sales
+    agg_df = filtered_df.groupby('Category', as_index=False)['Sales'].sum()
+    
+    # Create the figure using Plotly Express
+    fig = px.bar(agg_df, x='Category', y='Sales')
+    return fig
+
+
+def subCategory_sales_chart(region, state, city):
+    # Create a copy of the dataframe
+    filtered_df = df.copy()
+    # Filter by region, state, and city if provided
+    if region:
+        filtered_df = filtered_df[filtered_df['Region'].isin(region)]
+    if state:
+        filtered_df = filtered_df[filtered_df['State'].isin(state)]
+    if city:
+        filtered_df = filtered_df[filtered_df['City'].isin(city)]
+    
+    # Group by category and sum the sales
+    agg_df = filtered_df.groupby('Sub-Category', as_index=False)['Sales'].sum()
+    
+    # Create the figure using Plotly Express
+    fig = px.bar(agg_df, x='Sub-Category', y='Sales')
+    return fig
+
+
+def create_choropleth_map(region, state):
+    # Create a copy of the dataframe
+    filtered_df = df.copy()
+    # Filter by region, state, and city if provided
+    if region:
+        filtered_df = filtered_df[filtered_df['Region'].isin(region)]
+    if state:
+        filtered_df = filtered_df[filtered_df['State'].isin(state)] 
+    filtered_df['Code'] = filtered_df['State'].map(code)
+    filtered_df = filtered_df.groupby('Code', as_index=False)['Sales'].sum().round(2)
+
+    # Create a choropleth map
+    fig = px.choropleth(filtered_df, locations='Code', color='Sales', color_continuous_scale='spectral_r', hover_name='Code', locationmode='USA-states', labels={'State Sales'}, scope='usa')
+
+    # Update layout for better display
+    fig.update_layout(
+        title_text='Sales by US State',
+        geo=dict( scope='usa', lakecolor='rgb(255, 255, 255)', showlakes=True, showland=True, landcolor='rgb(217, 217, 217)', subunitcolor='rgb(255, 255, 255)'),
+        margin=dict(l=10,r=10,t=50,b=10))
+    return fig
